68fac4e3f9fbaa97158b6cdca6fc17015edab2fb
Ryouya matsushima <blue.conjrer@gmail.com>
2020-12-03 21:26:22 +0900

    add animation

--- js/common.js
+++ js/common.js
@@ -16,4 +16,15 @@ $(function(){
     },function(){
         $(this).children(".hoverBox").fadeOut();
     });
+    
 });
+
+// lottie
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('Lottie'), 
+    renderer: 'svg', // 描画形式
+    loop: true, // trueにしたらループ。1回再生の場合はfalse
+    autoplay: true, // 自動再生
+    path: 'loadingV2.json' ,// jsonのパスを指定
+    name: "Hello World", // 今後のリファレンスとして名前を指定（オプション）
+    });
